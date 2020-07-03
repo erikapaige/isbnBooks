42ba07b79021c990d59b7814cb3cfeb989428e99
Erika <erika.paige.handley@gmail.com>
2020-07-02 22:47:29 -0700

    models

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { model, Schema } = require('mongoose')
+
+const User = new Schema({
+  username: String,
+  email: String,
+  name: String,
+  books: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Book',
+    loanedto: String,
+    loanedEmail: String,
+    loanedPhone: String
+  }]
+})
+
+User.plugin(require('passport-local-mongoose'))
+
+module.exports = model('User', User)
